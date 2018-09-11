f158a8f60c6c65784d80754eceaac998038bcde7
Pedro Mayorga <pedrojmayorga@gmail.com>
2018-09-11 17:31:58 -0500

    version 2.0 Proyecto

--- src/main/java/edu/escuelaing/arem/mediaserver/socket/ServerSckt.java
+++ src/main/java/edu/escuelaing/arem/mediaserver/socket/ServerSckt.java
@@ -37,6 +37,6 @@ public class ServerSckt {
     if (System.getenv("PORT") != null) {
         return Integer.parseInt(System.getenv("PORT"));
     }
-    return 9999; //returns default port if heroku-port isn't set (i.e. on localhost)
+    return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 }
